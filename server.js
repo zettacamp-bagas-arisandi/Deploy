8af30006a284af99f28ffd73f99e0756fe71b6ea
Bagas Arisandi Hidayat <114066151+zettacamp-bagas-arisandi@users.noreply.github.com>
2022-11-28 21:19:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ async function start(typeDefs, resolvers){
 // function connect to db
 const connectDB = async () => {
     try {
-      await mongoose.connect(`mongodb+srv://notes:notes123@mini-project.slwlqew.mongodb.net/?retryWrites=true&w=majority`);
+      await mongoose.connect(`mongodb+srv://notes:notes123@mini-project.slwlqew.mongodb.net/zettacamp?retryWrites=true&w=majority`);
       console.log(`Connected to ${database}: ${portdb}`);
     } catch (err) {
       console.log('Failed to connect to MongoDB', err);
