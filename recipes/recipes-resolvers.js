82ca5fceacca8fa0b7ca254b70f7c9cfa9631565
Bagas Arisandi Hidayat <114066151+zettacamp-bagas-arisandi@users.noreply.github.com>
2022-12-15 09:03:38 +0700

    Add files via upload

--- recipes/recipes-resolvers.js
+++ recipes/recipes-resolvers.js
@@ -209,6 +209,7 @@ async function GetOneRecipes(parent, {id}){
 //////////////// MUTATION ////////////////
 async function CreateRecipes(parent, { recipe_name, input, description, price, image, status = 'unpublish', discount = 1, sold = 0, category} ){
         let check = await recipesModel.findOne({ recipe_name: new RegExp("^" + recipe_name.trim() + "$", 'i') });
+        console.log(check)
         if(check.status!=='deleted'){
             throw new GraphQLError(`${recipe_name} sudah ada!`);
         }
